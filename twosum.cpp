405a689e5a59cdf4764c0314829627765ab86736
Bhuvan.R <144478466+Thor-asgardian@users.noreply.github.com>
2024-04-24 22:52:16 +0530

    Create twosum.cpp

---
+++ twosum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // No solution found
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    Solution sol;
+    vector<int> result = sol.twoSum(nums, target);
+    if (result.size() == 2) {
+        cout << "Indices: " << result[0] << " " << result[1] << endl;
+    } else {
+        cout << "No solution found" << endl;
+    }
+    return 0;
+}
